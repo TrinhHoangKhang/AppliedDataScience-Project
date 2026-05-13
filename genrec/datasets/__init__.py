8c52d06e96138f52b6c24b7c5781ec1d5523874b
TrinhHoangKhang <trinhhoangkhangvl@gmail.com>
2026-05-13 19:57:50 +0700

    remove redundancy

--- genrec/datasets/__init__.py
+++ genrec/datasets/__init__.py
@@ -1,6 +1,3 @@
 from genrec.datasets.AmazonReviews2014.dataset import AmazonReviews2014
 from genrec.datasets.AmazonReviews2018.dataset import AmazonReviews2018
 from genrec.datasets.AmazonReviews2023.dataset import AmazonReviews2023
-from genrec.datasets.Steam.dataset import Steam
-from genrec.datasets.Yelp.dataset import Yelp
-from genrec.datasets.Synthetic.dataset import Synthetic
